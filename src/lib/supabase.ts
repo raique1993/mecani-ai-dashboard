97bbae83fb39b6b17af92829d5c20f0c842e682d
raique1993 <105059420+raique1993@users.noreply.github.com>
2026-05-25 11:11:36 -0300

    fix: add fallback supabase credentials

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://tcjynyfusqkqtdohnyzq.supabase.co'
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_gNJhc4WCS9MnnmDAL2T6Vg_AQ71_Vid'
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
